0dadec856c08beb2ccc925d732e9fa918d4dc984
hupratt <hprattdo@gmail.com>
2022-12-11 00:19:07 +0100

    try sockets basic file

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
+require("dotenv").config();
+
+const fs = require("fs");
 const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
-const port = process.env.PORT || 8080;
+const port = process.env.port || 8095;
+
+var options = {
+  key: fs.readFileSync(process.env.privateKey, "utf8"),
+  cert: fs.readFileSync(process.env.certificate, "utf8"),
+};
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+ssl = http = require(process.env.http).createServer(options, app);
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
+const io = require("socket.io")(http);
+
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
-});
-
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
