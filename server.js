97de20c1a1d770323b2149f98a95443c149d2dc2
hupratt <hprattdo@gmail.com>
2022-12-10 22:08:50 +0100

    revert

--- server.js
+++ server.js
@@ -316,7 +316,7 @@ if (isUseHTTPs) {
 }
 
 RTCMultiConnectionServer.beforeHttpListen(httpApp, config);
-httpApp = httpApp.listen(3000, "0.0.0.0", function () {
+httpApp = httpApp.listen(process.env.PORT, process.env.IP, function () {
   RTCMultiConnectionServer.afterHttpListen(httpApp, config);
 });
 
