d62341b10301ad7f8c96bd0ede3bd171ad7ee941
hupratt <hprattdo@gmail.com>
2022-12-10 21:34:38 +0100

    try again

--- server.js
+++ server.js
@@ -316,7 +316,7 @@ if (isUseHTTPs) {
 }
 
 RTCMultiConnectionServer.beforeHttpListen(httpApp, config);
-httpApp = httpApp.listen(process.env.PORT, process.env.IP, function () {
+httpApp = httpApp.listen("9001", "rtc.craftstudios.shop", function () {
   RTCMultiConnectionServer.afterHttpListen(httpApp, config);
 });
 
